eef41950bf9214d750310419c8bae81966201826
Amiad Bareli <hatul@hatul.info>
2012-08-21 21:17:46 +0300

    add cache

--- feed2podcast.php
+++ feed2podcast.php
@@ -1,25 +1,36 @@
 <?php
-function feed2pc($feed){
+function feed2pc($feed,$update_hours=5){
+	$update_seconds=$update_hours*3600;
 	//error_reporting(2147483647);
-	$feed_str=file_get_contents($feed);
-	$sxe = new SimpleXMLElement($feed_str);
-	foreach ($sxe->channel->item as $item){
-		$page=file_get_contents($item->link);
-		$dom = new DOMDocument();
-		@$dom->loadHTML($page);
-		$xpath = new DOMXPath($dom);
-		$hrefs = $xpath->evaluate("/html/body//a");
-		for ($i = 0; $i < $hrefs->length; $i++) {
-			$href = $hrefs->item($i);
-			$url = $href->getAttribute('href');
-			if(substr($url,-4)=='.mp3') {
-				$file=$url;
-				break;
+	$cach_file=str_replace('/','-',$feed);
+	if ((!file_exists($cach_file)) || ((time()-filemtime($cach_file)>$update_seconds))){
+		$feed_str=file_get_contents($feed);
+		$sxe = new SimpleXMLElement($feed_str);
+		foreach ($sxe->channel->item as $item){
+			$page=file_get_contents($item->link);
+			$dom = new DOMDocument();
+			@$dom->loadHTML($page);
+			$xpath = new DOMXPath($dom);
+			$hrefs = $xpath->evaluate("/html/body//a");
+			for ($i = 0; $i < $hrefs->length; $i++) {
+				$href = $hrefs->item($i);
+				$url = $href->getAttribute('href');
+				if(substr($url,-4)=='.mp3') {
+					$file=$url;
+					break;
+				}
 			}
+			$enclosure=$item->addChild('enclosure');
+			$enclosure->addAttribute('url',$file);
+			$enclosure->addAttribute('type','mp3');
 		}
-		$enclosure=$item->addChild('enclosure');
-		$enclosure->addAttribute('url',$file);
-		$enclosure->addAttribute('type','mp3');
+		$xml=$sxe->asXML();
+		file_put_contents($cach_file,$xml);
+		echo $xml;
 	}
-	echo $sxe->asXML();
+	else {
+		ob_clean();
+    		flush();
+	    	readfile($cach_file);
+	    }
 }
