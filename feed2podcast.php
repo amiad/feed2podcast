1fda340e89ff3d52aa63133d06cfec2fb12563ed
Amiad Bareli <hatul@hatul.info>
2012-08-21 21:57:17 +0300

    OOP by tomer

--- feed2podcast.php
+++ feed2podcast.php
@@ -1,36 +1,53 @@
 <?php
-function feed2pc($feed,$update_hours=5){
-	$update_seconds=$update_hours*3600;
-	//error_reporting(2147483647);
-	$cache_file='cache/'.str_replace('/','-',$feed);
-	if ((!file_exists($cache_file)) || ((time()-filemtime($cache_file)>$update_seconds))){
+
+class PodcastFeedCreator {
+	public function __construct($feed,$update_hours=5) {
+        	if ($this->get_cache($feed,$update_hours) return;
+        	else echo $this->processFeed($feed);
+    	}
+    
+	public function processFeed($feed) {
 		$feed_str=file_get_contents($feed);
 		$sxe = new SimpleXMLElement($feed_str);
-		foreach ($sxe->channel->item as $item){
-			$page=file_get_contents($item->link);
-			$dom = new DOMDocument();
-			@$dom->loadHTML($page);
-			$xpath = new DOMXPath($dom);
-			$hrefs = $xpath->evaluate("/html/body//a");
-			for ($i = 0; $i < $hrefs->length; $i++) {
-				$href = $hrefs->item($i);
-				$url = $href->getAttribute('href');
-				if(substr($url,-4)=='.mp3') {
-					$file=$url;
-					break;
-				}
-			}
+		foreach ($sxe->channel->item as $item) {
+			$file = $this->findEnclosureLink($item->link);
 			$enclosure=$item->addChild('enclosure');
 			$enclosure->addAttribute('url',$file);
 			$enclosure->addAttribute('type','mp3');
 		}
 		$xml=$sxe->asXML();
-		file_put_contents($cache_file,$xml);
-		echo $xml;
+		$this->save_cache($feed,$xml);
+		return $xml;
 	}
-	else {
-		ob_clean();
-    		flush();
-	    	readfile($cache_file);
-	    }
+    
+	public function findEnclosureLink($item) {
+        	$page=file_get_contents($item);
+		$dom = new DOMDocument();
+		@$dom->loadHTML($page);
+		$xpath = new DOMXPath($dom);
+		$hrefs = $xpath->evaluate("/html/body//a");
+		for ($i = 0; $i < $hrefs->length; $i++) {
+			$href = $hrefs->item($i);
+			$url = $href->getAttribute('href');
+			if(substr($url,-4)=='.mp3') {
+				return $url;
+			}
+        	}
+	}
+	private function get_cache($feed,$update_hours){
+    		$update_seconds=$update_hours*3600;
+    		$cache_file='cache/'.md5($feed);
+		if ((file_exists($cache_file)) && ((time()-filemtime($cache_file)<$update_seconds))){
+			ob_clean();
+    			flush();
+	    		readfile($cache_file);
+	    		return true;
+		}
+		else return false;
+	}
+	private function save_cache($feed,$xml){
+		$cache_file='cache/'.md5($feed);
+		file_put_contents($cache_file,$xml);
+	}
 }
+
