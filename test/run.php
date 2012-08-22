99ee6e1105964c7f1848154553198bf23a198dec
Amiad Bareli <hatul@hatul.info>
2012-08-22 12:21:29 +0300

    update test file

--- test/run.php
+++ test/run.php
@@ -1,3 +1,10 @@
 <?php
 require('../feed2podcast.php');
 $feed2podcast = new PodcastFeedCreator('test.atom');
+
+/* optional */
+$feed2podcast->setType('mp3'); //file type, default mp3
+$feed2podcast->setUpdateHours(1); //How often the cache is updated (in hours), default 5
+/* end optional */
+
+$feed2podcast->getFeed();
