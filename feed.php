d957f7d9363118f3a095d1b394e7d55bbbba8eab
Amiad Bareli <hatul@hatul.info>
2012-08-23 17:40:36 +0300

    add feed.php for example

---
+++ feed.php
@@ -0,0 +1,20 @@
+<?php
+
+/* required */
+$feed='http://news.php.net/group.php?group=php.announce&format=rss'; //url of the feed
+
+/* optional */
+$type=''; //file type, default mp3
+$updateHours=5; //How often the cache is updated (in hours), default 5
+$image=''; //podcast logo url, default none;
+$delStr=''; // delete substring in file url. for sites that redirect the download via other
+
+require('feed2podcast.php');
+$feed2podcast = new PodcastFeedCreator($feed);
+
+$feed2podcast->setType($type); 
+$feed2podcast->setUpdateHours($updateHours); 
+$feed2podcast->setImage($image); 
+$feed2podcast->setDelStr($delStr); 
+
+$feed2podcast->getFeed();
