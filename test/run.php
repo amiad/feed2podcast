6ad327b92421c7d2ca070fefc583aa9d15746de6
Amiad Bareli <hatul@hatul.info>
2012-08-23 11:36:43 +0300

    change test to rss

--- test/run.php
+++ test/run.php
@@ -1,12 +1,12 @@
 <?php
 require('../feed2podcast.php');
-$feed2podcast = new PodcastFeedCreator('test.atom');
+$feed2podcast = new PodcastFeedCreator('test.rss');
 
 /* optional */
 $feed2podcast->setType('mp3'); //file type, default mp3
 $feed2podcast->setUpdateHours(1); //How often the cache is updated (in hours), default 5
 $feed2podcast->setImage('https://www.google.com/images/srpr/logo3w.png'); //podcast logo url, default none;
-$feed2podcast->setDelStr(); // delete substring in file url. for sites that redirect the download via other. file.
+$feed2podcast->setDelStr(); // delete substring in file url. for sites that redirect the download via other file.
 /* end optional */
 
 $feed2podcast->getFeed();
