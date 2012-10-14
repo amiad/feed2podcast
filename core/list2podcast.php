10f0c0e869c365309ae4887098c7e37806fed383
Amiad Bareli <hatul@hatul.info>
2012-10-14 18:10:44 +0200

    add list2podcast

---
+++ core/list2podcast.php
@@ -0,0 +1,41 @@
+<?php
+require('convert2podcast.php');
+class PodcastCreatorFromList extends Convert2Podcast{
+    	
+	public function setTitle($title){
+		$this->title=$title;
+	}
+	public function setDesc($desc){
+		$this->desc=$desc;
+	}
+
+	public function processFeed() {
+		$list=$this->list;
+		$type=$this->type;
+		$lines=file($list);
+		$feed_str="<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+		<rss version=\"2.0\">
+		<channel>
+		<title>$this->title</title>
+		<description>$this->desc</description>
+		<link>$page</link>
+		</channel>
+		</rss>";
+		$sxe = new SimpleXMLElement($feed_str);
+		$sxe=$this->addImage($sxe);
+		foreach ($lines as $line) {
+			$list($file,$title)=explode("|",$line);
+			$item=$sxe->channel->addChild('item');
+			$item->addChild('title',$title);
+			$item->addChild('description',$title);
+			$item->addChild('link',$file);
+			$item->addChild('guid',$file);
+			$enclosure=$item->addChild('enclosure');
+			$enclosure->addAttribute('url',$file);
+			$enclosure->addAttribute('type',$this->mime(strtolower(substr($file,-3))));
+		}
+		$xml=$sxe->asXML();
+		$this->save_cache($xml);
+		return $xml;
+	}
+}
---
+++ core/list2podcast.php~
@@ -0,0 +1,38 @@
+<?php
+require('convert2podcast.php');
+class PodcastFeedCreator extends Convert2Podcast{
+	public function processFeed() {
+		$feed=$this->source;
+		$type=$this->type;
+		$feed_str=file_get_contents($feed);
+		$sxe = new SimpleXMLElement($feed_str);
+		foreach ($sxe->channel->item as $item) {
+			$file = $this->findEnclosureLink($item->link,$type);
+			if($this->inType($file)){
+				if($this->delStr) $file=str_replace($this->delStr,'',$file);
+				$enclosure=$item->addChild('enclosure');
+				$enclosure->addAttribute('url',$file);
+				$enclosure->addAttribute('type',$this->mime($this->inType($file)));
+			}
+		}
+		$sxe=$this->addImage($sxe);
+		$xml=$sxe->asXML();
+		$this->save_cache($xml);
+		return $xml;
+	}
+    
+	public function findEnclosureLink($item,$type) {
+		$page=file_get_contents($item);
+		$dom = new DOMDocument();
+		@$dom->loadHTML($page);
+		$xpath = new DOMXPath($dom);
+		$hrefs = $xpath->evaluate("/html/body//a");
+		for ($i = 0; $i < $hrefs->length; $i++) {
+			$href = $hrefs->item($i);
+			$url = $href->getAttribute('href');
+			if($this->inType($url)) {
+				return $url;
+			}
+		}
+	}
+}
